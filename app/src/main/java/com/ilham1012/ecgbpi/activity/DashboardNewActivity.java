3fe97990fe1b1d19f0800817030904c29636eb06
Muhammad Ilham <myung1012@gmaill.com>
2016-02-14 09:38:06 +0700

    Sync Adapter + Services

--- app/src/main/java/com/ilham1012/ecgbpi/activity/DashboardNewActivity.java
+++ app/src/main/java/com/ilham1012/ecgbpi/activity/DashboardNewActivity.java
@@ -1,5 +1,8 @@
 package com.ilham1012.ecgbpi.activity;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
@@ -28,6 +31,16 @@ public class DashboardNewActivity extends AppCompatActivity {
     private SessionManager session;
     private ListView listView;
     private SimpleCursorAdapter dataAdapter;
+
+    // Constants
+    // The authority for the sync adapter's content provider
+    public static final String AUTHORITY = "com.ilham1012.ecgbpi.provider";
+    // An account type, in the form of a domain name
+    public static final String ACCOUNT_TYPE = "ilham1012.com";
+    // The account name
+    public static final String ACCOUNT = "dummyaccount";
+    // Instance fields
+    Account mAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -184,6 +197,40 @@ public class DashboardNewActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
+    /**
+     * Create a new dummy account for the sync adapter
+     *
+     * @param context The application context
+     */
+    public static Account CreateSyncAccount(Context context) {
+        // Create the account type and default account
+        Account newAccount = new Account(
+                ACCOUNT, ACCOUNT_TYPE);
+        // Get an instance of the Android account manager
+        AccountManager accountManager =
+                (AccountManager) context.getSystemService(
+                        ACCOUNT_SERVICE);
+        /*
+         * Add the account and account type, no password or user data
+         * If successful, return the Account object, otherwise report an error.
+         */
+        if (accountManager.addAccountExplicitly(newAccount, null, null)) {
+            /*
+             * If you don't set android:syncable="true" in
+             * in your <provider> element in the manifest,
+             * then call context.setIsSyncable(account, AUTHORITY, 1)
+             * here.
+             */
+        } else {
+            /*
+             * The account exists or some other error occurred. Log this, report it,
+             * or handle it internally.
+             */
         }
     }
 
